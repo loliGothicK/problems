82ace8b42ab141cf3c52e98753d74ffc0b4cc3c3
Cranberries <yussa.de.dannann@gmail.com>
2016-02-12 03:02:26 +0900

    手直し

--- problem_4/enum_ans/interface.hpp
+++ problem_4/enum_ans/interface.hpp
@@ -7,71 +7,71 @@ namespace Interface{
     public:
         short operator()(short const& l, short const& r) { return T::Calculate(l,r); }
     };
-	//interface of Addition
-	class A : public callable<A>
+    //interface of Addition
+    class A : public callable<A>
     {
-	public:
+    public:
         //default ctor
         constexpr A() = default;
-		~A() = default;
-		//calculate function object
-		static short Calculate(short const& l, short const& r)
-		{
+	~A() = default;
+	//calculate function object
+	static short Calculate(short const& l, short const& r)
+	{
 			return l + r ;
-		} ;
-	} ;
+	}
+    } ;
 
-	//interface of Subtraction
-	class B : public callable<B>
-	{
-	public:
-        constexpr B() = default;
-		~B() = default;
-
-		static short Calculate(short const& l, short const& r)
-		{
-			return l - r ;
-		} ;
-	} ;
-
-	//interface of Multiplication
-	class C : public callable<C>
-	{
-	public:
-		constexpr C() = default;
-		~C() = default;
-
-		static short Calculate(short const& l, short const& r)
-		{
-			return l * r ;
-		} ;
-	} ;
-
-	//interface of Division
-	class D : public callable<D>
-	{
-	public:
-        constexpr D() = default;
-		~D() = default;
-
-		static short Calculate(short const& l, short const& r)
-		{
-			return l / r ;
-		} ;
-	} ;
-
-	//interface of Special Operator
-	class E : public callable<E>
+    //interface of Subtraction
+    class B : public callable<B>
     {
-	public:
+    public:
+        constexpr B() = default;
+	~B() = default;
+
+	static short Calculate(short const& l, short const& r)
+	{
+			return l - r ;
+	}
+    } ;
+
+    //interface of Multiplication
+    class C : public callable<C>
+    {
+    public:
+	constexpr C() = default;
+	~C() = default;
+
+	static short Calculate(short const& l, short const& r)
+	{
+		return l * r ;
+	}
+    } ;
+
+    //interface of Division
+    class D : public callable<D>
+    {
+    public:
+        constexpr D() = default;
+	~D() = default;
+
+	static short Calculate(short const& l, short const& r)
+	{
+		return l / r ;
+	}
+    } ;
+
+    //interface of Special Operator
+    class E : public callable<E>
+    {
+    public:
         constexpr E() = default;
-		~E() = default;
+	~E() = default;
 
         static short Calculate(short const& l, short const& r)
-		{
-			return l + r + 5 ;
-		} ;
-	} ;
+	{
+		return l + r + 5 ;
+	}
+    } ;
 
 
 }
